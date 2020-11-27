c9ec41e9bbf830f5a4749804c539659d4702bd3a
hugonza4 <hugonza4@gmail.com>
2020-11-27 17:06:16 +0100

    clean average code commentary

--- average.cs
+++ average.cs
@@ -8,17 +8,17 @@ public class Average {
 		string s;
 		
 		Console.Write("Insert a mark: ");
-		s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-		while(s!="end") {  // if the mark isn't an end then we pass it to double to work with it
+		s=Console.ReadLine();		 
+		while(s!="end") {  
 			b=Convert.ToDouble(s);
-			c++;   //counter of numbers
-			a+=b;  // sum of all the notes
+			c++;   
+			a+=b; 
 			Console.Write("Insert a mark: ");
 			s=Console.ReadLine();
 		}
 
 		if(c!=0){
-			d=a/c;  //calculate the average
+			d=a/c; 
 			Console.WriteLine("The average is {0}.",d);
 		}
 	}
